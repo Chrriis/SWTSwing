fd28a38a5ac1bde279ff0dd40e371f1443953670
Chrriis <chrriis@gmail.com>
2007-07-27 21:23:36 +0000

    Compatibility with Java 4 is now supported.

--- SWTSwing/Eclipse SWT/swing/org/eclipse/swt/widgets/FileDialog.java
+++ SWTSwing/Eclipse SWT/swing/org/eclipse/swt/widgets/FileDialog.java
@@ -186,12 +186,18 @@ public String open () {
           if(f.isDirectory()) {
             return true;
           }
-          if(!Compatibility.IS_JAVA_5_OR_GREATER) {
-            return true;
-          }
           for(int i=0; i<filters.length; i++) {
             String filterExtension = filters[i].trim();
-            String regExp = "\\Q" + filterExtension.replace("\\E", "\\\\E").replace("\\Q", "\\\\Q").replace("?", "\\E.\\Q").replace("*", "\\E.*\\Q");
+            String regExp;
+            if(!Compatibility.IS_JAVA_5_OR_GREATER) {
+              filterExtension = Pattern.compile("\\E").matcher(filterExtension).replaceAll("\\\\E");
+              filterExtension = Pattern.compile("\\Q").matcher(filterExtension).replaceAll("\\\\Q");
+              filterExtension = Pattern.compile("?").matcher(filterExtension).replaceAll("\\E.\\Q");
+              filterExtension = Pattern.compile("*").matcher(filterExtension).replaceAll("\\E.*\\Q");
+              regExp = "\\Q" + filterExtension;
+            } else {
+              regExp = "\\Q" + filterExtension.replace("\\E", "\\\\E").replace("\\Q", "\\\\Q").replace("?", "\\E.\\Q").replace("*", "\\E.*\\Q");
+            }
             final Pattern pattern = Pattern.compile(regExp.toString());
             Matcher m = pattern.matcher(f.getName());
             if(m.matches()) {

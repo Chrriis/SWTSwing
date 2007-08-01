41b36a09fafb056edb94c1e816ce5d304ec0b4ec
Chrriis <chrriis@gmail.com>
2007-08-01 17:51:45 +0000

    Few additional transfers are implemented (but not tested...)

--- SWTSwing/Eclipse SWT Drag and Drop/swing/org/eclipse/swt/dnd/HTMLTransfer.java
+++ SWTSwing/Eclipse SWT Drag and Drop/swing/org/eclipse/swt/dnd/HTMLTransfer.java
@@ -12,6 +12,7 @@ package org.eclipse.swt.dnd;
 
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.io.Reader;
 
 /**
  * The class <code>TextTransfer</code> provides a platform specific mechanism 
@@ -27,33 +28,32 @@ import java.awt.datatransfer.StringSelection;
  * 
  * @see Transfer
  */
-public class TextTransfer extends Transfer {
+public class HTMLTransfer extends Transfer {
 
-private static TextTransfer _instance = new TextTransfer();
+private static HTMLTransfer _instance = new HTMLTransfer();
 	
-private TextTransfer() {}
+private HTMLTransfer() {}
 
 /**
  * Returns the singleton instance of the TextTransfer class.
  *
  * @return the singleton instance of the TextTransfer class
  */
-public static TextTransfer getInstance () {
+public static HTMLTransfer getInstance () {
 	return _instance;
 }
 
 /**
- * This implementation of <code>javaToNative</code> converts plain text
+ * This implementation of <code>javaToNative</code> converts HTML-formatted text
  * represented by a java <code>String</code> to a platform specific representation.
+ * For additional information see <code>Transfer#javaToNative</code>.
  * 
- * @param object a java <code>String</code> containing text
- * @param transferData an empty <code>TransferData</code> object; this object
- *  will be filled in on return with the platform specific format of the data
- *  
- * @see Transfer#javaToNative
+ * @param object a java <code>String</code> containing HTML text
+ * @param transferData an empty <code>TransferData</code> object; this
+ *  object will be filled in on return with the platform specific format of the data
  */
 public void javaToNative (Object object, TransferData transferData){
-	if (!checkText(object) || !isSupportedType(transferData)) {
+	if (!checkHTML(object) || !isSupportedType(transferData)) {
 		DND.error(DND.ERROR_INVALID_DATA);
 	}
   transferData.transferable = new StringSelection((String)object);
@@ -61,33 +61,54 @@ public void javaToNative (Object object, TransferData transferData){
 
 /**
  * This implementation of <code>nativeToJava</code> converts a platform specific 
- * representation of plain text to a java <code>String</code>.
+ * representation of HTML text to a java <code>String</code>.
+ * For additional information see <code>Transfer#nativeToJava</code>.
  * 
- * @param transferData the platform specific representation of the data to be converted
- * @return a java <code>String</code> containing text if the conversion was successful; otherwise null
- * 
- * @see Transfer#nativeToJava
+ * @param transferData the platform specific representation of the data to be 
+ * been converted
+ * @return a java <code>String</code> containing HTML text if the 
+ * conversion was successful; otherwise null
  */
 public Object nativeToJava(TransferData transferData){
-  return super.nativeToJava(transferData);
+  Object o = super.nativeToJava(transferData);
+  if(o instanceof Reader) {
+    StringBuilder sb = new StringBuilder();
+    Reader reader = (Reader)o;
+    char[] chars = new char[128];
+    try {
+      for(int i; (i=reader.read(chars)) != -1; ) {
+        sb.append(chars, 0, i);
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    return sb.toString();
+  }
+  return o;
 }
 
-boolean checkText(Object object) {
+boolean checkHTML(Object object) {
 	return (object != null  && object instanceof String && ((String)object).length() > 0);
 }
 
 protected boolean validate(Object object) {
-	return checkText(object);
+	return checkHTML(object);
 }
 
+static DataFlavor HTML_FLAVOR_1 = new DataFlavor("text/html; class=java.lang.String", "HTML Text");
+//static DataFlavor HTML_FLAVOR_2 = new DataFlavor("text/html; class=java.io.Reader", "HTML Text");
+
 public TransferData[] getSupportedTypes() {
-  TransferData data = new TransferData();
-  data.dataFlavor = DataFlavor.stringFlavor;
-  return new TransferData[] {data};
+  TransferData data1 = new TransferData();
+  data1.dataFlavor = HTML_FLAVOR_1;
+  return new TransferData[] {data1};
+//  TransferData data2 = new TransferData();
+//  data2.dataFlavor = HTML_FLAVOR_2;
+//  return new TransferData[] {data1, data2};
 }
 
 protected DataFlavor getDataFlavor() {
-  return DataFlavor.stringFlavor;
+  return HTML_FLAVOR_1;
 }
 
 }
